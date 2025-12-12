1115ca4b4a9ca35dea32fac8b93eb504da8b45a2
akbar <aaban562@gmail.com>
2025-12-12 18:50:04 +0500

    feat: add swiper

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -5,43 +5,83 @@ import Logo from "../../assets/images/logo.png";
 import { GrMenu } from "react-icons/gr";
 import { FaUser } from "react-icons/fa";
 import { MdShoppingBasket } from "react-icons/md";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Header: React.FC = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  useEffect(() => {
+    document.body.style.overflow = isSidebarOpen ? "hidden" : "";
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isSidebarOpen]);
+
   return (
     <>
-      <div className="header container">
-        <div className="header__actions">
+      <header className="header container">
+        <div className="header__actions header__actions-desktop">
           <Link to={"/home"} className="header__actions__logo">
             <img src={Logo} alt="Logo" />
           </Link>
         </div>
 
-        <div className="header__navs">
+        <nav className="header__navs header__navs-desktop">
           <NavLink to={"/home"}>
-            <FaUser style={{ fontSize: "1.5rem", color: "grey" }} />
+            <FaUser className="header__icon" />
             <span>Войти</span>
           </NavLink>
 
           <NavLink to={"/home"}>
-            <MdShoppingBasket style={{ fontSize: "1.5rem", color: "grey" }} />
+            <MdShoppingBasket className="header__icon" />
             <span>Корзина</span>
           </NavLink>
-        </div>
+        </nav>
 
         <div className="header__actions header__actions-mobile">
           <Link to={"/home"} className="header__actions__logo">
             <img src={Logo} alt="Logo" />
           </Link>
 
-          <GrMenu
-            className="header__actions__icon"
+          <button
+            className="header__burger"
+            aria-label="Открыть меню"
             onClick={() => setIsSidebarOpen(true)}
-          />
+          >
+            <GrMenu className="header__actions__icon" />
+          </button>
         </div>
-      </div>
+      </header>
+
+      <div
+        className={`overlay ${isSidebarOpen ? "overlay--open" : ""}`}
+        onClick={() => setIsSidebarOpen(false)}
+      />
+
+      <aside className={`sidebar ${isSidebarOpen ? "sidebar--open" : ""}`}>
+        <div className="sidebar__top">
+          <span className="sidebar__title">Меню</span>
+          <button
+            className="sidebar__close"
+            aria-label="Закрыть меню"
+            onClick={() => setIsSidebarOpen(false)}
+          >
+            ✕
+          </button>
+        </div>
+
+        <nav className="sidebar__navs">
+          <NavLink to={"/home"} onClick={() => setIsSidebarOpen(false)}>
+            <FaUser className="header__icon" />
+            <span>Войти</span>
+          </NavLink>
+
+          <NavLink to={"/home"} onClick={() => setIsSidebarOpen(false)}>
+            <MdShoppingBasket className="header__icon" />
+            <span>Корзина</span>
+          </NavLink>
+        </nav>
+      </aside>
     </>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,65 @@
 import type React from "react";
-import "./Home.css"
+import "./Home.css";
+
 import CButton from "../../components/CButton";
 import CInput from "../../components/CInput";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+const slide_img = [
+  "https://images.unsplash.com/photo-1517336714731-489689fd1ca8",
+  "https://images.unsplash.com/photo-1585386959984-a4155224a1ad",
+  "https://images.unsplash.com/photo-1542291026-7eec264c27ff",
+  "https://images.unsplash.com/photo-1512496015851-a90fb38ba796",
+  "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f",
+  "https://images.unsplash.com/photo-1556906781-9a412961c28c",
+];
 
 const Home: React.FC = () => {
   return (
     <>
       <h1>Home</h1>
-      <CButton title="Click me"></CButton>
-      <CInput></CInput>
+
+      <div>
+        <Swiper
+          effect="creative"
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView="auto"
+          loop={true}
+          spaceBetween={24}
+          navigation={true}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          modules={[Navigation, Autoplay]}
+          creativeEffect={{
+            prev: {
+              translate: ["-120%", 0, -300],
+              rotate: [0, 0, -20],
+            },
+            next: {
+              translate: ["120%", 0, -300],
+              rotate: [0, 0, 20],
+            },
+          }}
+          className="mySwiper"
+        >
+          {slide_img.map((img, item) => {
+            return (
+              <SwiperSlide key={item}>
+                <img src={img} alt="" />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
     </>
   );
 };
